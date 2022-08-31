7fee16fcc42de22b996ddd7b93ada319b4d71b48
Douglas Wardell-Johnson <Douglaswj@gmail.com>
2022-08-31 20:29:17 +0100

    Add Morrone's final years

--- dicaprio-gfs/dicaprio-gfs.R
+++ dicaprio-gfs/dicaprio-gfs.R
@@ -11,8 +11,8 @@ showtext_auto()
 
 #create main dataframe 
 df<-data.frame(
-  year = 1999:2019,
-  age_leo = 24:44,
+  year = 1999:2022,
+  age_leo = 24:47,
   gf = c(
     rep("Gisele Bundchen",6),
     rep("Bar Refaeli",6),
@@ -21,22 +21,22 @@ df<-data.frame(
     rep("Toni Garrn",2),
     "Kelly Rohrbach",
     rep("Nina Agdal",2),
-    rep("Camila Morrone",2)
+    rep("Camila Morrone",5)
   ),
   age_gf = c(
     18:23,
-    20:25, 
-    23, 
-    22, 
+    20:25,
+    23,
+    22,
     20:21,
     25,
-    24:25, 
-    20:21
+    24:25,
+    21:25
   )
 )
 
 #data for annotations about max age limit
-max_points<-data.frame(x=c(2010, 2015, 2017), y=rep(25,3))
+max_points<-data.frame(x=c(2010, 2015, 2017,2022), y=rep(25,4))
 
 #data for year segments by girlfriend
 by_gf<-df|>
@@ -53,17 +53,20 @@ pal_annotate<-'#B6B6B6'
 
 #data for images and respective positions on x axis
 images<-data.frame(name=c("Leonardo DiCaprio", unique(df$gf)),
-                   pos = seq(from=1999, to=2019, length.out = 9),
+                   pos = seq(from=1999, to=2022, length.out = 9),
                    pal_label<-c(pal_leo, rep(pal_gf,8)))|>
   mutate(path = paste0("images/",str_replace_all(tolower(name)," ","_"),".png"))
 
 
 #data frame for connector segments from images to years
 connectors<-data.frame(
-  x= c(2001.5,2004,2005.5,2006.5, 2006.5,2011,2009,2009,2012,2011.5,2013.5,2014,2015,2016.5,2019,2018.5),
-  xend = c(2001.5, 2005.5,2005.5,2006.5,2011,2011,2009,2012,2012,2013.5,2013.5,2015,2015,2016.5,2018.5,2018.5),
-  y= c(-10,-10,-10,-10,-6,-6,-10,-8,-8,-10,-10,-10,-10,-10,-10,-10),
-  yend= c(-4,-10,-4,-6,-6,-4,-8,-8,-4,-10,-4,-10,-4,-4,-10,-4)
+  #x= c(2001.5,2004,2005.5,2006.5, 2006.5,2011,2009,2009,2012,2011.5,2013.5,2014,2015,2016.5,2019,2018.5),
+  x= c(2001.625,2004.25,2005.5,2006.875, 2006.875,2011,2009.5,2009.5,2012,2012.125,2013.5,2014.75,2015,2017.375,2016.5,2020,2020),
+  #x=images$pos[2:length(images$pos)],
+  xend = c(2001.625, 2005.5,2005.5,2006.875,2011,2011,2009.5,2012,2012,2013.5,2013.5,2015,2015,2016.5,2016.5,2020,2020),
+  #xend = images$pos[2:length(images$pos)],
+  y= c(-10,-10,-10,-10,-6,-6,-10,-8,-8,-10,-10,-10,-10,-10,-10,-10,-10),
+  yend= c(-4,-10,-4,-6,-6,-4,-8,-8,-4,-10,-4,-10,-4,-10,-4,-10,-4)
 )
 
 #create custom title to use with ggtext::element_textbox_simple
@@ -72,7 +75,7 @@ title<-'<span style="color:#FD7600;">LEONARDO DICAPRIO</span><span style="color:
 #plot
 ggplot(data=df, aes(x=year))+
   geom_segment(data = data.frame(y = seq(from=0, to=50, by=5)),
-               mapping=aes(x=1999, xend=2019, y=y, yend=y), color="white", size=0.1, alpha=0.2)+
+               mapping=aes(x=1999, xend=2023, y=y, yend=y), color="white", size=0.1, alpha=0.2)+
   #leos age data
   geom_line(mapping=aes(y=age_leo), color=pal_leo)+
   geom_point(mapping=aes(y=age_leo), shape=21, fill=pal_bg, color=pal_leo, size=4)+
@@ -86,10 +89,10 @@ ggplot(data=df, aes(x=year))+
   geom_text(mapping=aes(x=year, label=paste0("'",substr(year,3,4)), y=-1.5), color="white")+
   #max age limit annotations
   annotate(geom="text", label="Leo's Age Limit", x=2013.5, y=32, color=pal_annotate)+
-  geom_segment(mapping=aes(x=2010, xend=2017, y=30, yend=30), color=pal_annotate, size=0.15)+
+  geom_segment(mapping=aes(x=2010, xend=2022, y=30, yend=30), color=pal_annotate, size=0.05)+
   geom_segment(data=max_points, mapping=aes(x=x, xend=x, y=30, yend=26), color=pal_annotate, size=0.3)+
   geom_point(data=max_points, mapping=aes(x=x, y=y+1.5),
-             shape=21, fill=pal_bg, color=pal_annotate, size=11)+
+             shape=21, fill=pal_bg, color=pal_annotate, size=6)+
   geom_text(data=max_points, mapping=aes(x=x, y=y+1.5, label=y), color=pal_gf)+
   #custom legend
   geom_segment(data=data.frame(x=rep(2000,2), xend=rep(2002,2), y=c(40,45), color=c(pal_leo,pal_gf), size=c(0.25,1.5)),
@@ -108,8 +111,12 @@ ggplot(data=df, aes(x=year))+
   geom_segment(data=connectors, mapping=aes(x=x,xend=xend,y=y,yend=yend), color=pal_gf)+
   #plot images
   geom_image(data=images, mapping=aes(y=-14, x=pos, image=path), size=0.07)+
-  geom_richtext(data=images, 
-                mapping=aes(y=-19.5, x=pos, color=pal_label, label=str_replace(name," ","<br>")),
+  geom_richtext(data=images,
+                mapping=aes(y=-19.5, x=pos, color=pal_label, 
+                            label=str_replace(name," ","<br>"),
+                            #label=str_c('<p style="line-height:5%">',str_replace(name," ","<br>"),'</p>'),
+                            lineheight = .5
+                            ),
                 fill = NA, label.color = NA, hjust=0.4,
                 show.legend = FALSE, fontface="bold")+
   scale_color_identity()+
@@ -117,7 +124,7 @@ ggplot(data=df, aes(x=year))+
   theme(
     panel.background = element_rect(fill=pal_bg, color=NA),
     plot.background = element_rect(fill=pal_bg),
-    plot.title = element_textbox_simple(size=17, halign=0.5),
+    plot.title = element_textbox_simple(size=25, halign=0.5),
     text = element_text(color="white"),
     plot.margin = margin(t=30, l=10, r=10),
     panel.grid = element_blank(),
@@ -128,4 +135,4 @@ ggplot(data=df, aes(x=year))+
   )
 
 #save plot
-ggsave("plot/dicaprio-gfs.png", height=8, width=8)
+ggsave("plot/dicaprio-gfs.png", height=8, width=8,scale=0.58)
